2ad93face5524b7e8a62963aa2540ac0b5562a62
Oleksii Aleksandrov <pupkin21081994@gmail.com>
2017-10-10 20:54:21 +0200

    test

--- Jump way/Assets/Scripts/CubeJump.cs
+++ Jump way/Assets/Scripts/CubeJump.cs
@@ -2,14 +2,45 @@
 using System.Collections;
 
 public class CubeJump : MonoBehaviour {
-
     public GameObject mainCube;
+    private bool animate;
+    private float compressSpeed = 0.5f;
 
-	void OnMouseDown () {
-        mainCube.transform.position = new Vector3(mainCube.transform.position.x, mainCube.transform.position.y+0.5f, mainCube.transform.position.z);
+    private void FixedUpdate()
+    {
+        if(animate && mainCube.transform.localScale.y > 0.5f)
+        {
+            mainCube.transform.localPosition -= new Vector3(0.0f, compressSpeed * Time.deltaTime, 0.0f);
+            mainCube.transform.localScale -= new Vector3(0.0f, compressSpeed * Time.deltaTime, 0.0f);
+        }
+        else if (!animate)
+        {
+            if(mainCube.transform.localScale.y < 1.0f)
+            {
+                mainCube.transform.localPosition += new Vector3(0.0f, compressSpeed * Time.deltaTime * 2.0f, 0.0f);
+                mainCube.transform.localScale += new Vector3(0.0f, compressSpeed * Time.deltaTime * 2.0f, 0.0f);
+            }
+                else if(mainCube.transform.localScale.y != 1.0f)
+            {
+                mainCube.transform.localScale += new Vector3(1.0f, 1.0f, 1.0f);
+            }
+        }
+    }
+
+    void OnMouseDown ()
+    {
+        if (mainCube.GetComponent<Rigidbody>())
+        {
+            animate = true;
+        }
+        //mainCube.transform.position = new Vector3(mainCube.transform.position.x, mainCube.transform.position.y+0.5f, mainCube.transform.position.z);
 	}
 	
-	void OnMouseUp() {
-	
-	}
+	void OnMouseUp()
+    {
+        if (mainCube.GetComponent<Rigidbody>())
+        {
+            animate = false;
+        }
+    }
 }
